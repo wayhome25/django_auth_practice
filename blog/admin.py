5a68ebc6f11c340c7762dc908f6e8e9ea212359b
siwa.lee <siwa.lee@buzzvil.com>
2018-07-09 19:25:52 +0900

    간단한 blog 프로젝트 생성

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['title']
+    search_fields = ['title']
